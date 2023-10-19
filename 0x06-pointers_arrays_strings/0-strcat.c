6b4278d5a801dad1798e1c17497be5f2655f755f
Tabitha <114776922+TabithaMuhiukia@users.noreply.github.com>
2023-10-19 16:38:36 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,11 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
-*_strcat - concatenates two strings
-*
-*@dest: destination string
-*
-*@src: source string
-*
-*Return: pointer to dest
-=======
 *_strcat - function that concatenates two strings
 *@src: source of the strings
 *@dest: destination of the concatenated strings
 *
 *Return: Pointer to dest
->>>>>>> d15b9d0b86644fc4ae6ce7798a749ad0155b67fa
 */
 
 char *_strcat(char *dest, char *src)
