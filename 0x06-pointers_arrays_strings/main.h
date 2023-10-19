ee1c4e7463e27f6308b4051f62fed5a5d7523e37
TabithaMuhiukia <tabitha.muhiukia@gmail.com>
2023-10-19 09:54:58 +0300

    my main file

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+<<<<<<< HEAD
+void print_numner(int n);
+=======
+void print_number(int n);
+>>>>>>> d15b9d0b86644fc4ae6ce7798a749ad0155b67fa
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+char *rot13(char *);
+
+#endif
