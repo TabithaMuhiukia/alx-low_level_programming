5a88c95a98a2d5cb015851c06f869facd39a9c8a
TabithaMuhiukia <tabitha.muhiukia@gmail.com>
2023-10-20 05:46:01 +0300

    corrections

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# Example:./100-textme.rb 'Feb 1 11:00:00 ip-10-0-64-10 mdr: 2016-02-01 11:00:00 Receive SMS [SMSC:SYBASE2] [SVC:] [ACT:] [BINF:] [FID:] [from:+17272713208] [to:+19172319348] [flags:-1:0:-1:0:-1] [msg:136:Orbiting this at a distance of roughly ninety-two million miles is an utterly insignificant little blue green planet whose ape-descended] [udh:0:]'
+
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(',')
